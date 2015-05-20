d67f325e18343e3ac0727b1b5fddc93f681bb7a3
Richard Beare <Richard.Beare@ieee.org>
2015-05-20 12:24:05 +1000

    corrected subref

--- cg_mantis_template.m
+++ cg_mantis_template.m
@@ -19,55 +19,55 @@ dep=cdep;
 
 cdep (end+1) = cfg_dep;
 cdep(end).sname      = 'Neonate template channel 1';
-cdep(end).src_output = substruct('.','tissuemap','()',{'1'});
+cdep(end).src_output = substruct('.','tissuemap','()',{1});
 cdep(end).tgt_spec   = cfg_findspec({{'filter','image','strtype','e'}});
 dep=cdep;
 
 cdep (end+1) = cfg_dep;
 cdep(end).sname      = 'Neonate template channel 2';
-cdep(end).src_output = substruct('.','tissuemap','()',{'2'});
+cdep(end).src_output = substruct('.','tissuemap','()',{2});
 cdep(end).tgt_spec   = cfg_findspec({{'filter','image','strtype','e'}});
 dep=cdep;
 
 cdep (end+1) = cfg_dep;
 cdep(end).sname      = 'Neonate template channel 3';
-cdep(end).src_output = substruct('.','tissuemap','()',{'3'});
+cdep(end).src_output = substruct('.','tissuemap','()',{3});
 cdep(end).tgt_spec   = cfg_findspec({{'filter','image','strtype','e'}});
 dep=cdep;
 
 cdep (end+1) = cfg_dep;
 cdep(end).sname      = 'Neonate template channel 4';
-cdep(end).src_output = substruct('.','tissuemap','()',{'4'});
+cdep(end).src_output = substruct('.','tissuemap','()',{4});
 cdep(end).tgt_spec   = cfg_findspec({{'filter','image','strtype','e'}});
 dep=cdep;
 
 cdep (end+1) = cfg_dep;
 cdep(end).sname      = 'Neonate template channel 5';
-cdep(end).src_output = substruct('.','tissuemap','()',{'5'});
+cdep(end).src_output = substruct('.','tissuemap','()',{5});
 cdep(end).tgt_spec   = cfg_findspec({{'filter','image','strtype','e'}});
 dep=cdep;
 
 cdep (end+1) = cfg_dep;
 cdep(end).sname      = 'Neonate template channel 6';
-cdep(end).src_output = substruct('.','tissuemap','()',{'6'});
+cdep(end).src_output = substruct('.','tissuemap','()',{6});
 cdep(end).tgt_spec   = cfg_findspec({{'filter','image','strtype','e'}});
 dep=cdep;
 
 cdep (end+1) = cfg_dep;
 cdep(end).sname      = 'Neonate template channel 7';
-cdep(end).src_output = substruct('.','tissuemap','()',{'7'});
+cdep(end).src_output = substruct('.','tissuemap','()',{7});
 cdep(end).tgt_spec   = cfg_findspec({{'filter','image','strtype','e'}});
 dep=cdep;
 
 cdep (end+1) = cfg_dep;
 cdep(end).sname      = 'Neonate template channel 8';
-cdep(end).src_output = substruct('.','tissuemap','()',{'8'});
+cdep(end).src_output = substruct('.','tissuemap','()',{8});
 cdep(end).tgt_spec   = cfg_findspec({{'filter','image','strtype','e'}});
 dep=cdep;
 
 cdep (end+1) = cfg_dep;
 cdep(end).sname      = 'Neonate template channel 9';
-cdep(end).src_output = substruct('.','tissuemap','()',{'9'});
+cdep(end).src_output = substruct('.','tissuemap','()',{9});
 cdep(end).tgt_spec   = cfg_findspec({{'filter','image','strtype','e'}});
 dep=cdep;
 
