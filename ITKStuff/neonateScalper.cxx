ad88576fe4abee800daeedc430b1e9fab40d2e2a
Richard Beare <richard.beare@monash.edu>
2015-08-19 11:50:44 +1000

    simpler version of scalper for testing

--- ITKStuff/neonateScalper.cxx
+++ ITKStuff/neonateScalper.cxx
@@ -39,11 +39,13 @@ public:
 typedef class CmdLineType
 {
 public:
-  std::string InputIm, OutputIm; //
+  std::string InputIm, OutputIm, OutputMaskIm;
+  float erodesize, gradsize;
+  float merodesize, mdilatesize;
 } CmdLineType;
 
-bool debug=true;
-std::string debugprefix="/tmp/rb/prem";
+bool debug=false;
+std::string debugprefix="/tmp/prem";
 std::string debugsuffix=".nii.gz";
 
 template <class TImage>
@@ -71,6 +73,23 @@ void ParseCmdLine(int argc, char* argv[],
     ValueArg<std::string> outArg("o","output","output image",true,"result","string");
     cmd.add( outArg );
 
+    ValueArg<std::string> maskArg("m","mask","output brain mask",false,"","string");
+    cmd.add( maskArg );
+
+    ValueArg<float> erodeArg("", "erode", "size of erosion (mm)", false, 2, "float");
+    cmd.add(erodeArg);
+
+    ValueArg<float> gradArg("", "grad", "size of gradient smoothing (mm)", false, 1, "float");
+    cmd.add(gradArg);
+
+    ValueArg<float> merodeArg("", "markererode", "size of erosion (mm) used when creating marker", 
+                              false, 5, "float");
+    cmd.add(merodeArg);
+
+    ValueArg<float> mdilateArg("", "markerdilate", "size of dilation (mm) used when creating marker", 
+                               false, 10, "float");
+    cmd.add(mdilateArg);
+
     SwitchArg debugArg("d", "debug", "save debug images", debug);
     cmd.add( debugArg );
 
@@ -79,6 +98,13 @@ void ParseCmdLine(int argc, char* argv[],
 
     CmdLineObj.InputIm = inArg.getValue();
     CmdLineObj.OutputIm = outArg.getValue();
+    CmdLineObj.OutputMaskIm = maskArg.getValue();
+    debug = debugArg.getValue();
+    CmdLineObj.erodesize = erodeArg.getValue();
+    CmdLineObj.gradsize = gradArg.getValue();
+    CmdLineObj.merodesize = merodeArg.getValue();
+    CmdLineObj.mdilatesize = mdilateArg.getValue();
+
     }
   catch (ArgException &e)  // catch any exceptions
     {
@@ -88,115 +114,6 @@ void ParseCmdLine(int argc, char* argv[],
 
 //////////////////////////////////////////////////////////////
 #include "vibes_common.h"
-/////////////////////////////////////////////////////////////////
-template <class RawImType>
-typename RawImType::Pointer scaleSpaceSmooth(typename RawImType::Pointer input, std::vector<float> scales)
-{
-  // smooth at different scales and take a max
-  typename RawImType::Pointer res, tmp;
-
-  itk::Instance< itk::SmoothingRecursiveGaussianImageFilter< RawImType, RawImType > > Smoother;
-  itk::Instance< itk::MaximumImageFilter<RawImType, RawImType, RawImType> > Max;
-
-  if (scales.size() == 0)
-    {
-    std::cerr << "At least one smoothing scale must be specified" << std::endl;
-    return(0);
-    }
-  Smoother->SetInput(input);
-  Smoother->SetNormalizeAcrossScale(true);
-  Smoother->SetSigma(scales[0]);
-  res = Smoother->GetOutput();
-  res->Update();
-  res->DisconnectPipeline();
-
-  for (unsigned i=1; i<scales.size(); i++)
-    {
-    Smoother->SetSigma(scales[i]);
-    Max->SetInput(res);
-    Max->SetInput2(Smoother->GetOutput());
-    tmp=Max->GetOutput();
-    tmp->Update();
-    tmp->DisconnectPipeline();
-    res=tmp;
-    }
-  return(res);
-}
-/////////////////////////////////////////////////////////////////
-template <class MaskImType>
-typename MaskImType::Pointer binDilate(typename MaskImType::Pointer mask, float radius)
-{
-  itk::Instance<itk::BinaryDilateParaImageFilter<MaskImType> > Dilater;
-  Dilater->SetInput(mask);
-  Dilater->SetRadius(radius);
-  Dilater->SetUseImageSpacing(true);
-  typename MaskImType::Pointer result = Dilater->GetOutput();
-  result->Update();
-  result->DisconnectPipeline();
-  return(result);
-}
-/////////////////////////////////////////////////////////////////
-template <class RawImType, class MaskImType>
-typename MaskImType::Pointer doRefine(typename RawImType::Pointer raw, 
-					typename MaskImType::Pointer mask,
-					float gap)
-{
-  typedef typename itk::Image<float, RawImType::ImageDimension> FloatImType;
-  typedef typename FloatImType::Pointer FIPtr;
-  // 
-  itk::Instance<itk::BinaryErodeParaImageFilter<MaskImType> > Eroder;
-  Eroder->SetInput(mask);
-  Eroder->SetRadius(gap);
-  Eroder->SetUseImageSpacing(true);
-
-  // gradient from inner marker
-  itk::Instance<itk::DirectionalGradientImageFilter<RawImType, MaskImType, 
-						    FloatImType> > DirGrad;
-
-  DirGrad->SetInput(raw);
-  DirGrad->SetMaskImage(Eroder->GetOutput());
-  DirGrad->SetOutsideValue(1);
-  DirGrad->SetScale(-1);
-
-  std::vector<float> scales;
-  scales.push_back(0.25);
-  scales.push_back(0.5);
-  FIPtr grad = scaleSpaceSmooth<FloatImType>(DirGrad->GetOutput(), scales);
-
-
-  writeImDbg<FloatImType>(grad, "dgrad");
-  itk::Instance<itk::BinaryDilateParaImageFilter<MaskImType> > Dilater;
-  Dilater->SetInput(mask);
-  Dilater->SetRadius(gap);
-  Dilater->SetUseImageSpacing(true);
-  itk::Instance<itk::BinaryThresholdImageFilter<MaskImType, MaskImType> > Inverter;
-  Inverter->SetInput(Dilater->GetOutput());
-  Inverter->SetUpperThreshold(1);
-  Inverter->SetLowerThreshold(1);
-  Inverter->SetInsideValue(0);
-  Inverter->SetOutsideValue(2);
-
-  itk::Instance< itk::MaximumImageFilter <MaskImType, MaskImType, MaskImType> > Combine;
-  Combine->SetInput(Eroder->GetOutput());
-  Combine->SetInput2(Inverter->GetOutput());
-  
-  itk::Instance< itk::MorphologicalWatershedFromMarkersImageFilter<FloatImType, MaskImType> > WSFilt;
-  WSFilt->SetInput(grad);
-  WSFilt->SetMarkerImage(Combine->GetOutput());
-
-  itk::Instance<itk::BinaryThresholdImageFilter<MaskImType, MaskImType> > SelectBrain;
-  SelectBrain->SetInput(WSFilt->GetOutput());
-  SelectBrain->SetUpperThreshold(1);
-  SelectBrain->SetLowerThreshold(1);
-  SelectBrain->SetInsideValue(1);
-  SelectBrain->SetOutsideValue(0);
-
-  typename MaskImType::Pointer result = SelectBrain->GetOutput();
-  result->Update();
-  result->DisconnectPipeline();
-  return(result);
-
-}
 
 /////////////////////////////////////////////////////////////////
 template <class RawImType, class MaskImType>
@@ -255,81 +172,6 @@ typename MaskImType::Pointer findMarker(typename RawImType::Pointer raw,
   return(result);
 }
 /////////////////////////////////////////////////////////////////
-template <class RawImType>
-void findTop(typename RawImType::Pointer raw, float slice, 
-	     int &outtop, int &x, int &y)
-{
-  typedef typename itk::Image<unsigned char, RawImType::ImageDimension> MaskImType;
-
-  itk::Instance<itk::OtsuThresholdImageFilter <RawImType, MaskImType> > Thresh;
-  itk::Instance<itk::BinaryShapeKeepNObjectsImageFilter<MaskImType> > discarder;
-  Thresh->SetInput(raw);
-  Thresh->SetInsideValue(0);
-  Thresh->SetOutsideValue(1);
-  discarder->SetNumberOfObjects(1);
-  discarder->SetInput(Thresh->GetOutput());
-  discarder->SetForegroundValue(1);
-
-  typename MaskImType::Pointer mask = discarder->GetOutput();
-  mask->Update();
-  mask->DisconnectPipeline();
-
-  // find the top
-  typedef typename itk::BinaryImageToShapeLabelMapFilter<MaskImType> LabellerType;
-  typename LabellerType::Pointer labeller = LabellerType::New();
-
-  labeller->SetInput(mask);
-  labeller->SetFullyConnected(true);
-  labeller->SetInputForegroundValue(1);
-
-  typedef typename LabellerType::OutputImageType LabelMapType;
-  typedef typename LabellerType::OutputImagePointer LabelMapPointerType;
-  typedef typename LabelMapType::LabelObjectType LabelObjectType;
-
-  writeImDbg<MaskImType>(mask, "cropmask");
-
-  LabelMapPointerType labmap = labeller->GetOutput();
-  labmap->Update();
-  labmap->DisconnectPipeline();
-
-  // can only be one object
-  LabelObjectType * labelObject = labmap->GetLabelObject(1);
-  typename MaskImType::RegionType bb = labelObject->GetBoundingBox();
-
-
-  typename RawImType::SpacingType sp = raw->GetSpacing();
-  int top = bb.GetIndex()[2] + bb.GetSize()[2] - 1;
-  outtop=top;
-  {
-  // estimate the centroid using the top 15mm
-  typedef typename itk::LabelImageToShapeLabelMapFilter<MaskImType> LabellerTypeB;
-  typename LabellerTypeB::Pointer labellerB = LabellerTypeB::New();
-
-  typedef typename LabellerTypeB::OutputImageType LabelMapTypeB;
-  typedef typename LabellerTypeB::OutputImagePointer LabelMapPointerTypeB;
-  typedef typename LabelMapTypeB::LabelObjectType LabelObjectTypeB;
-
-  typename MaskImType::RegionType blank = mask->GetLargestPossibleRegion();
-  typename MaskImType::RegionType::SizeType s = blank.GetSize();
-  s[2] = top - slice/sp[2];
-  blank.SetSize(s);
-
-  fillRegion<MaskImType>(mask, blank, 0);
-  writeImDbg<MaskImType>(mask, "topmask");
-  labellerB->SetInput(mask);
-  LabelMapPointerTypeB labmapB = labellerB->GetOutput();
-  labmapB->Update();
-  labmapB->DisconnectPipeline();
-  LabelObjectTypeB * labelObjectB = labmapB->GetLabelObject(1);
-  typename MaskImType::PointType cent = labelObjectB->GetCentroid();
-  typename MaskImType::IndexType ind;
-  mask->TransformPhysicalPointToIndex(cent, ind);
-  x=ind[0];
-  y=ind[1];
-  }
-
-}
-/////////////////////////////////////////////////////////////////
 template <class PixType, int dimension>
 void doSeg(const CmdLineType &CmdLineObj)
 {
@@ -337,23 +179,24 @@ void doSeg(const CmdLineType &CmdLineObj)
   typedef itk::Image<PixType, dimension> ImageType;
   typedef typename ImageType::Pointer IPtr;
   typedef typename itk::Image<float, ImageType::ImageDimension> FloatImType;
-  typedef typename FloatImType::Pointer FIPtr;
+  //typedef typename FloatImType::Pointer FIPtr;
   typedef typename itk::Image<unsigned char, ImageType::ImageDimension> MaskImType;
   typedef typename MaskImType::Pointer MIPtr;
 
   IPtr T2orig = readIm<ImageType>(CmdLineObj.InputIm);
 
   // do everything on eroded images, then dilate the result
-  float erad = 3.0;
+  float erad = CmdLineObj.erodesize;
   IPtr T2 = doErodeMM<ImageType>(T2orig, erad);
 
-  MIPtr marker = findMarker<ImageType, MaskImType>(T2, 5, 10);
+  MIPtr marker = findMarker<ImageType, MaskImType>(T2, CmdLineObj.merodesize, CmdLineObj.mdilatesize);
   writeImDbg<MaskImType>(marker, "marker");
-  
+  writeImDbg<ImageType>(T2, "eroded");
+
   // see if we can get away with a simple gradient.
   itk::Instance <itk::GradientMagnitudeRecursiveGaussianImageFilter<ImageType, FloatImType> > GradFilt;
   GradFilt->SetInput(T2);
-  GradFilt->SetSigma(3);
+  GradFilt->SetSigma(CmdLineObj.gradsize);
 
   itk::Instance< itk::MorphologicalWatershedFromMarkersImageFilter<FloatImType, MaskImType> > WSFilt;
   WSFilt->SetInput(GradFilt->GetOutput());
@@ -367,14 +210,12 @@ void doSeg(const CmdLineType &CmdLineObj)
   SelectBrain->SetInsideValue(1);
   SelectBrain->SetOutsideValue(0);
 
-  
-
-  writeImDbg<MaskImType>(doDilate<MaskImType>(SelectBrain->GetOutput(), erad), "phase1");
+  writeImDbg<MaskImType>(SelectBrain->GetOutput(), "phase1");
   // Now for a phase 2?? Use a thinner gradient, with local
   // orientation so that we pick up a decrease in intensity only
 
-  MIPtr NewMask = doRefine<ImageType, MaskImType>(T2, SelectBrain->GetOutput(), 3);
-  writeIm<MaskImType>(doDilate<MaskImType>(NewMask, erad), CmdLineObj.OutputIm);
+  //MIPtr NewMask = doRefine<ImageType, MaskImType>(T2, SelectBrain->GetOutput(), 3);
+  writeIm<MaskImType>(doDilateMM<MaskImType>(SelectBrain->GetOutput(), erad), CmdLineObj.OutputIm);
 
 
 }
@@ -385,7 +226,18 @@ int main(int argc, char * argv[])
   ParseCmdLine(argc, argv, CmdLineObj);
 
   const int dimension = 3;
-
+  int dim1 = 0;
+  itk::ImageIOBase::IOComponentType ComponentType;
+  if (!readImageInfo(CmdLineObj.InputIm, &ComponentType, &dim1)) 
+    {
+    std::cerr << "Failed to open " << CmdLineObj.InputIm << std::endl;
+    return(EXIT_FAILURE);
+    }
+  if (dim1 != dimension) 
+    {
+      std::cerr << CmdLineObj.InputIm << "isn't 3D" << std::endl;
+      return(EXIT_FAILURE);
+    }
   // These tolerances are being set high because we rely on spm to pass in
   // appropriate data. Problems arise because spm uses the sform when
   // the sform and qform are different while ITK seems to use the
@@ -397,8 +249,21 @@ int main(int argc, char * argv[])
   itk::ImageToImageFilterCommon::SetGlobalDefaultCoordinateTolerance(1000.0);
   itk::ImageToImageFilterCommon::SetGlobalDefaultDirectionTolerance(1000.0);
 
-  doSeg<short, dimension>(CmdLineObj);
-
+  switch (ComponentType) 
+    {
+    case (itk::ImageIOBase::SHORT):
+      doSeg<short, dimension>(CmdLineObj);
+      break;
+    case (itk::ImageIOBase::USHORT):
+      doSeg<unsigned short, dimension>(CmdLineObj);
+      break;
+    case (itk::ImageIOBase::INT):
+      doSeg<int, dimension>(CmdLineObj);
+      break;
+    default:
+      doSeg<float, dimension>(CmdLineObj);
+      break;
+    }
 
   return(EXIT_SUCCESS);
 }
